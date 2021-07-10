0370b6d7f1042b6de01526ab424dad48a3a9d7af
Süheyla Kabak <74386450+KediOtu@users.noreply.github.com>
2021-07-11 00:33:03 +0300

    Update Program.cs

--- Paint/Paint/Program.cs
+++ Paint/Paint/Program.cs
@@ -17,6 +17,7 @@ namespace Paint
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //Github için deneme yorum satırı
         }
     }
 }
